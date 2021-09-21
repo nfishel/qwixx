abb406218c32c02e9a5134561387692a02ebd05d
Mr. Fishel <nfishel@hse.k12.in.us>
2021-09-21 11:18:26 -0400

    start js

--- js/score.js
+++ js/score.js
@@ -1,1 +1,29 @@
 console.log("Let's Play Qwixx!!")
+
+
+//          [2,3,4,5,6,7,8,9,10,11,12,loc]
+//          [0,1,2,3,4,5,6,7,8, 9, 10,11]
+const red = [0,0,0,0,0,0,0,0,0,0,0,0];
+const yellow = [0,0,0,0,0,0,0,0,0,0,0,0];
+const green = [0,0,0,0,0,0,0,0,0,0,0,0];
+const blue = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+
+function handelClick(event){
+    color = event.target.dataset.col;
+    loc = parseInt(event.target.dataset.loc) - 2;
+    // console.log('clicked '+ event.target.dataset.col+ " - " + event.target.dataset.loc);
+    event.target.innerHTML = 'X';
+    if(color == 'red' && red[loc] == 0){
+        red[loc] = "X";
+    }
+    for(let i = loc-1; i>=0; i--){
+        if(red[i] == 0){
+            red[i] = '-';
+        }
+    }
+    console.log(red);
+}
+
+const boxes = document.querySelectorAll(".box");
+boxes.forEach((box)=>  box.addEventListener('click',handelClick));
